42c6261416b121a26ed07d524a14aaaeb08f0f68
Aguimar Neto <aguimar@gmail.com>
2018-10-11 20:55:28 -0300

    Correcao de bug

--- clashapi.py
+++ clashapi.py
@@ -30,7 +30,7 @@ class ClashApiClient:
         # TODO _tag must be parameter
         self._tag = '%23VY28C0GJ'
         
-        self._key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU3YjNkOTgyLTdmMzAtNGUxNy05YTQyLWQ2NjAzNDliYjcyNyIsImlhdCI6MTUzOTIxNjk5MCwic3ViIjoiZGV2ZWxvcGVyL2M3ZDFkNzIzLTI1MDYtMDc4MS1kMmUzLWRjZmZiN2M3OGQzNCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzcuOTguMTc3LjIyMSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.mpimO-Aph9xFTUh7WOBgBLu0NsixMUvli2MYCsn2LfF4Vl4rx-738Ow1RtpStc7PMmugLK-7UOHD52GJ4F1p-w'
+        self._key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIzY2QyZTdlLTE1MzEtNGU1YS1hNDQ0LTg4YTcwM2Q3YjAwMCIsImlhdCI6MTUzOTMwMTQwMywic3ViIjoiZGV2ZWxvcGVyL2M3ZDFkNzIzLTI1MDYtMDc4MS1kMmUzLWRjZmZiN2M3OGQzNCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODYuMjEzLjYxLjEwMyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.VQ2eIj8HlqwJEi0okS_Vx3mCkm8RN-r5rZ0WlEllLTNYebqbe8lx2KDn-OxlO1DITaLYWwgR2fezRK0BcJi_jA'
         self._base_url = self.clashapi.base_url
         self._headers = {
                     "Accept":"Aplication/json",
@@ -47,4 +47,4 @@ class ClashApiClient:
         return r
 
     def get_player_json(self, parameters):
-        return json.dumps(self.get_player_info(parameters))
+        return self.get_player_info(parameters).json()
