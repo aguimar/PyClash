58544ae4c38335e1cd14142f8f921518ff0a1c49
Aguimar Neto <aguimar@gmail.com>
2018-10-20 18:44:34 -0300

    V0.1 funcionando

--- keygen.py
+++ keygen.py
@@ -3,6 +3,7 @@ import requests
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 import time
 
 def wait(fn):
@@ -21,7 +22,8 @@ def wait(fn):
 def wait_for(fn):
         return fn()
 
-url_clash_api = 'https://developer.clashroyale.com/#/login'
+url_clash_api_login = 'https://developer.clashroyale.com/#/login'
+url_clash_api_new_key = 'https://developer.clashroyale.com/#/new-key' 
 
 login = 'aguimar@gmail.com'
 senha = 'mat9807632' # tirar isso daqui
@@ -34,19 +36,25 @@ print(my_ip)
 
 browser = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
 
-browser.get(url_clash_api)
-
-
+browser.get(url_clash_api_new_key)
 browser.find_element_by_id('email').send_keys(login)
 browser.find_element_by_id('password').send_keys(senha)
 
 browser.find_element_by_id('password').send_keys(Keys.ENTER)
-browser.switch_to.active_element
 
+time.sleep(15)
+
+print('Current URL -->' + browser.current_url)
 # TODO https://developer.clashroyale.com/#/account
 # Contar quantas chaves existem 
 
-browser.get('https://developer.clashroyale.com/#/new-key')
+#browser.get(url_clash_api_new_key)
+browser.get('http://developer.clashroyale.com/#/new-key')
+
+#time.sleep(60)
+time.sleep(15)
+print('Current URL -->' + browser.current_url)
+
 #browser.find_element_by_class_name('btn.btn-default.dropdown-toggle').submit()
 #browser.find_element_by_css_selector('button.btn btn-default dropdown-toggle').submit()
 browser.find_element_by_id('name').send_keys(my_ip)
@@ -54,20 +62,19 @@ browser.find_element_by_id('description').send_keys(my_ip)
 browser.find_element_by_id('range-0').send_keys(my_ip)
 browser.find_element_by_id('range-0').send_keys(Keys.ENTER)
 
+time.sleep(15)
 browser.get('https://developer.clashroyale.com/#/account')
 content = browser.find_elements_by_class_name('api-key')
 content = browser.find_elements_by_css_selector('li.api-key')
-link = browser.find_elements_by_tag_name('a')
+link = browser.find_elements_by_xpath('//a[contains(@href, "key")]')
 # Abrir a nova chave e copiar o
 
 key = content[-1]
-browser.get('https://developer.clashroyale.com/#/key/' + key)
+key.click()
 
-temp = browser.find_element_by_link_text('Aguimar Neto').click()
-temp = browser.find_element_by_link_text('My Account')
-temp.send_keys(Keys.ENTER)
-browser.find_element_by_link_text('Create New Key').click()
+hash = browser.find_element_by_xpath('//samp')
 
+print(hash.text)
 
 
 
