7d6ab0a82f596fcf55219434aa20de616d4ed4eb
Aguimar Neto <aguimar@gmail.com>
2018-10-29 20:53:29 -0300

    Testando nova api para retrieve dos dados

--- program.py
+++ program.py
@@ -7,12 +7,12 @@ from clashapi import ClashApiClient
 import datetime
 
 clashapi_handler = ClashApiClient()
-clash_repository = PlayerStatRepository('sqlite:///Stats.db')
+clash_repository = PlayerStatRepository('sqlite:///StatsRoyaleAPI.db')
 
-api_return = clashapi_handler.get_player_json('players', '%23VY28C0GJ')
+api_return = clashapi_handler.get_player_info('VY28C0GJ')
 
-playerStat = PlayerStat(name = api_return['name'], trophies = api_return['trophies'], 
-                                wins = api_return['wins'], losses = api_return['losses'], date = datetime.datetime.now())
+playerStat = PlayerStat(name = api_return._boxed_data['name'], trophies = api_return._boxed_data['trophies'], 
+                                wins = api_return._boxed_data['games']['wins'], losses = api_return._boxed_data['games']['losses'], date = datetime.datetime.now())
 
 clash_repository.add(playerStat)
 clash_repository.save()
